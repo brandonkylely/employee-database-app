5543d78b56dde4981f7d515e5263a66fa9d4980d
Brandon <brandonkly@ucla.edu>
2022-12-18 18:05:01 -0800

    added view and add queries, connected db, created library structure

---
+++ index.js
@@ -0,0 +1,59 @@
+const inquirer = require('inquirer');
+
+const departments = require('./lib/departments')
+const employees = require('./lib/employees')
+const roles = require('./lib/roles')
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);
+
+function startApplication() {
+  return inquirer.prompt([
+    {
+        type: 'list',
+        name: 'action',
+        message: "What would you like to do?",
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+        ]
+    }
+])
+.then(({action}) => {
+    switch(action) {
+        case 'View all departments':
+            viewDepartments();
+            break;
+        case 'View all roles':
+            viewRoles();
+            break;
+        case 'View all employees':
+            viewEmployees();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;
+        case 'Add a role':
+            addRole();
+            break;
+        case 'Add an employee':
+            addEmployee();
+            break;
+        default: 
+            console.log('Something went wrong.');
+    }
+});
+}
+
+startApplication()
